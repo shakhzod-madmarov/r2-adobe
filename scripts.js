5fa29886cb1e608f34c153368a55da4c8892463a
SHAKHZOD <113131580+shakhzod-madmarov@users.noreply.github.com>
2024-01-29 08:15:49 +0500

    done

--- scripts.js
+++ scripts.js
@@ -11,4 +11,31 @@ const swiper = new Swiper(".mySwiper", {
     delay: 2500,
     disableOnInteraction: false,
   },
+  breakpoints: {
+    100: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2,
+      },
+    },
+    600: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+      },
+    },
+    1100: {
+      slidesPerView: 4,
+      grid: {
+        rows: 2,
+      },
+    },
+  },
+});
+
+const btnNav = document.querySelector(".nav_icon-btn");
+const nav = document.querySelector(".nav");
+
+btnNav.addEventListener("click", () => {
+  nav.classList.toggle("active");
 });
